50af7f102d16fb33aeb22cb7732534ef95466f7a
Kevin <1041874421@qq.com>
2017-03-09 16:33:30 +0800

    事件处理器 1.没有看组件的例子

--- step-1/app.js
+++ step-1/app.js
@@ -240,3 +240,36 @@ var app10 = new Vue({
         }
     }
 })
+var example1 = new Vue({
+  el: '#example-2',
+  data: {
+    items: [
+      {message: 'Foo' },
+      {message: 'Bar' }
+    ]
+  }
+})
+var example2 = new Vue({
+  el: '#example-3',
+  data: {
+    parentMessage: 'Parent',
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+// v-for 可以通过一个对象属性来迭代
+new Vue({
+  el: '#repeat-object',
+  data: {
+    object: {
+      FirstName: 'John',
+      LastName: 'Doe',
+      Age: 30
+    }
+  }
+})
+new Vue({
+    el: "#one"
+})
--- step-1/bundle.js
+++ step-1/bundle.js
@@ -284,6 +284,33 @@
 	    }
 	  }
 	});
+	var example1 = new _vue2.default({
+	  el: '#example-2',
+	  data: {
+	    items: [{ message: 'Foo' }, { message: 'Bar' }]
+	  }
+	});
+	var example2 = new _vue2.default({
+	  el: '#example-3',
+	  data: {
+	    parentMessage: 'Parent',
+	    items: [{ message: 'Foo' }, { message: 'Bar' }]
+	  }
+	});
+	// v-for 可以通过一个对象属性来迭代
+	new _vue2.default({
+	  el: '#repeat-object',
+	  data: {
+	    object: {
+	      FirstName: 'John',
+	      LastName: 'Doe',
+	      Age: 30
+	    }
+	  }
+	});
+	new _vue2.default({
+	  el: "#one"
+	});
 
 /***/ },
 /* 1 */
