4d60a8dc0984aed6638ececf58dc8148b9f5b4ae
Kevin <1041874421@qq.com>
2017-03-11 11:44:57 +0800

    检测数据的双向绑定

--- step-2/app.js
+++ step-2/app.js
@@ -6,9 +6,15 @@ bar();
 var app = new Vue({
   el: '#app', // 页面元素 可类和ID  但是如果类出现两次，第二次不会操作DOM上去
   data: {
-    message: '你好!',
-    text: '白小霁',
-    time:1
+    newTodo: '',
+    todoList:[]
+  },
+  created:function(){
+    let i = 1;
+    setInterval(() => {
+      this.newTodo = i;
+      i++;
+    },1000)
   }
 })
 
